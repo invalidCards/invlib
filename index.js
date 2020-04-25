798e9d938eea2b45824e56bc5433420948bfed49
invalidCards <842080+invalidCards@users.noreply.github.com>
2020-04-25 19:40:22 +0200

    Rework to circumvent flaws with raw packets

--- index.js
+++ index.js
@@ -11,54 +11,54 @@ bot.once("ready", () => {
     console.log(`Bot ready at ${(new Date()).toUTCString()}, serving ${bot.guilds.cache.size} guilds with ${bot.channels.cache.size} channels and ${bot.users.cache.size} users.`);
 });
 
-bot.on("raw", async packet => {
-    if (["HELLO", "READY", "RESUMED", "RECONNECT", "INVALID_SESSION"].includes(packet.t)) {
-        return; //These are internal events, and no functionality should be hooked into them through raw events
-    }
-
-    if (packet.t === "MESSAGE_CREATE" && packet.d.type === 0 && packet.d.content.startsWith(botConfig.prefix + "help")) {
-        let allFileContents = new Map();
-        let header = "```md\nCOMMAND LIST\n\n";
-        let footer = "```";
-        let cmdlist = "";
-        let longest = -1;
-        for (let file of fs.readdirSync(getPath()).filter(file => file.endsWith(".json"))) {
-            let fileContents = JSON.parse(fs.readFileSync(getPath(file)));
-            if (!fileContents.command) { continue; }
-            allFileContents.set(file, fileContents);
-            let compareString = botConfig.prefix + file.split(".")[0];
-            if (compareString.length > longest) {longest = compareString.length;}
-        }
-        for (let fc of allFileContents) {
-            cmdlist += `<${(botConfig.prefix + fc[0].split(".")[0]).padEnd(longest)} ${fc[1].desc}>\n`
-        }
-        let channel = await bot.channels.fetch(packet.d.channel_id, true);
-        if (channel && ['dm', 'text'].includes(channel.type)) {
-            channel.send(header + cmdlist + footer);
-        }
-    } else {
-        for (let file of fs.readdirSync(getPath()).filter(file => file.endsWith(".json"))) {
-            fs.readFile(getPath(file), (err, data) => {
-                if (err) {
-                    console.error(`Internal error: ${err.message}`)
-                } else {
-                    try {
-                        let config = JSON.parse(data);
-                        if (config.command && packet.t === "MESSAGE_CREATE" && packet.d.type === 0 && packet.d.content.startsWith(botConfig.prefix + file.split(".")[0])) {
-                            let args = packet.d.content.split(" ");
-                            args.shift();
-                            rnc(getCounterpartPath(file)).execute(bot, packet, args);
-                        } else if (!config.command && config.events.includes(packet.t)) {
-                            rnc(getCounterpartPath(file)).execute(bot, packet);
-                        }
-                    } catch(catcherr) {
-                        console.warn(`Error parsing ${file} to JSON, skipping - ${catcherr.message}`);
-                    }
+bot.on('message', (msg) => {
+    if (msg.cleanContent.startsWith(botConfig.prefix) && msg.cleanContent.replace(botConfig.prefix).split(' ')[0] === 'help') {
+        let configFiles = fs.readdirSync(getPath()).filter(item => item.endsWith('.json'));
+        let longestCmd = -1;
+        let longestFeat = -1;
+        for (let file of configFiles) {
+            let contents = JSON.parse(fs.readFileSync(file));
+            
+            if (contents.command) {
+                let compareString = botConfig + file;
+                if (compareString.length > longestCmd) {
+                    longestCmd = compareString.length;
                 }
-            });
+            } else {
+                let compareString = file;
+                if (compareString.length > longestFeat) {
+                    longestCmd = compareString.length;
+                }
+            }
         }
+        let header = '```md\n# COMMANDS\n\n';
+        let midder = '# FEATURES\n\n';
+        let footer = '```';
+        let cmdMessage = '';
+        let featMessage = '';
+        for (let file of configFiles) {
+            let contents = JSON.parse(fs.readFileSync(file));
+            if (contents.command) {
+                cmdMessage += `<${(botConfig.prefix + file).padEnd(longestCmd)} ${contents.desc}>\n`;
+            } else {
+                featMessage += `<${file.padEnd(longestFeat)} ${contents.desc}>\n`;
+            }
+        }
+        msg.channel.send(header + cmdMessage + midder + featMessage + footer);
+    } else {
+        callModules('message', msg);
     }
 });
+
+function callModules(event, ...args) {
+    let configFiles = fs.readdirSync(getPath()).filter(item => item.endsWith('.json'));
+    for (let file of configFiles) {
+        let contents = JSON.parse(fs.readFileSync(file));
+        if ((contents.command && event === 'message' && args[0].cleanContent.startsWith(botConfig.prefix)) || contents.events.includes(event)) {
+            rnc(getCounterpartPath(file)).execute(event, bot, args);
+        }
+    }
+}
 
 function rnc(module) {
     delete require.cache[require.resolve(module)];
@@ -67,9 +67,9 @@ function rnc(module) {
 
 function getPath(filename) {
     if (filename) {
-        return path.join(arp.toString(), "functions", filename);
+        return path.join(arp.toString(), "modules", filename);
     } else {
-        return path.join(arp.toString(), "functions");
+        return path.join(arp.toString(), "modules");
     }
 }
 
@@ -89,7 +89,7 @@ module.exports.run = (config) => {
         process.exit(-1);
     }
 
-    for (let file of fs.readdirSync(path.join(arp.toString(), "functions"))) {
+    for (let file of fs.readdirSync(path.join(arp.toString(), "modules"))) {
         if (file.endsWith(".js") && !fs.existsSync(getCounterpartPath(file))) {
             console.warn(`File ${file} does not have an associated .json (config) file, and its functionality will never run.`);
         } else if (file.endsWith(".json") && !fs.existsSync(getCounterpartPath(file))) {
